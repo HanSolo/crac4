26bcf66f2eb44a25d3ae81808c5fafa74e31c887
Gerrit Grunwald <han.solo@mac.com>
2022-04-08 12:36:35 +0200

    Cosmetics

--- src/main/java/eu/hansolo/crac4/Main.java
+++ src/main/java/eu/hansolo/crac4/Main.java
@@ -133,5 +133,9 @@ public class Main implements Resource {
     public static void main(String[] args) {
         Runtime runtime = Runtime.getRuntime();
         Main main = new Main(runtime);
+        
+        try {
+            while (true) { Thread.sleep(1000); }
+        } catch (InterruptedException e) { }
     }
 }
