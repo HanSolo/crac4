4b67e811ac9414081935ad366d0f45bd62fc46bc
Gerrit Grunwald <han.solo@mac.com>
2022-04-21 09:33:30 +0200

    Cosmetics

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Output timestamp
-echo "$(date + '%H:%M:%S.%3N')"
+echo "$(date +'%H:%M:%S.%3N') Call jar file"
 
 # Output epoch seconds before starting the application
 #java -XX:+UnlockDiagnosticVMOptions -Djdk.crac.trace-startup-time=true -XX:+CRTraceStartupTime -XX:CRaCCheckpointTo=/home/hansolo/crac-files -jar build/libs/crac4-17.0.0.jar
