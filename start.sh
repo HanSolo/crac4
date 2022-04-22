dd0b55046f0ebf6e59b2cce7b22250038d440dc1
Gerrit Grunwald <han.solo@mac.com>
2022-04-22 12:39:41 +0200

    Make start.sh usable

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 # Output epoch seconds before starting the application
 #java -XX:+UnlockDiagnosticVMOptions -Djdk.crac.trace-startup-time=true -XX:+CRTraceStartupTime -XX:CRaCCheckpointTo=/home/hansolo/crac-files -jar build/libs/crac4-17.0.0.jar
-java -XX:CRaCCheckpointTo=/home/hansolo/crac-files -jar build/libs/crac4-17.0.0.jar
+java -jar build/libs/crac4-17.0.0.jar
