a51e2665cc2eb6356dcdd4ae292ac1f14ab32c3e
Gerrit Grunwald <han.solo@mac.com>
2022-04-22 12:44:12 +0200

    latest modifications from main branch

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Output timestamp
+echo "$(date +'%H:%M:%S.%3N') Call jar file"
+
 # Output epoch seconds before starting the application
 #java -XX:+UnlockDiagnosticVMOptions -Djdk.crac.trace-startup-time=true -XX:+CRTraceStartupTime -XX:CRaCCheckpointTo=/home/hansolo/crac-files -jar build/libs/crac4-17.0.0.jar
+#java -XX:CRaCCheckpointTo=/home/hansolo/crac-files -jar build/libs/crac4-17.0.0.jar
 java -jar build/libs/crac4-17.0.0.jar
