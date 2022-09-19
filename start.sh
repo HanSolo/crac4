4b0ecf670ef4c18308db0926f601714e0dc2b7d3
Gerrit Grunwald <han.solo@mac.com>
2022-09-19 18:01:29 +0200

    cosmetics

--- start.sh
+++ start.sh
@@ -10,18 +10,18 @@ echo "$(date +'%H:%M:%S.%3N') Call jar file"
 # java -XX:CRaCCheckpointTo=crac-files -XX:+UnlockDiagnosticVMOptions -XX:+LogCompilation -jar build/libs/crac4-17.0.0.jar
 
 # Check whether crac-files folder is present not empty and if so, restore from checkpoint, otherwise start from jar
-if [ -d "crac-files" ]; then
-  if [ "$(ls -A crac-files)" ]; then
+if [ -d "/home/hansolo/crac-files" ]; then
+  if [ "$(ls -A /home/hansolo/crac-files)" ]; then
     echo "Restore from checkpoint"
-    java -XX:CRaCRestoreFrom=crac-files
+    java -XX:CRaCRestoreFrom=/home/hansolo/crac-files
   else
     echo "Standard start from jar file"
-    java -XX:CRaCCheckpointTo=crac-files -jar build/libs/crac4-17.0.0.jar
+    java -XX:CRaCCheckpointTo=/home/hansolo/crac-files -jar build/libs/crac4-17.0.0.jar
   fi
 else
   echo "No crac-files folder found"
-  mkdir crac-files
+  mkdir /home/hansolo/crac-files
   echo "Standard start from jar file"
-  java -XX:CRaCCheckpointTo=crac-files -jar build/libs/crac4-17.0.0.jar
+  java -XX:CRaCCheckpointTo=/home/hansolo/crac-files -jar build/libs/crac4-17.0.0.jar
 fi
 
