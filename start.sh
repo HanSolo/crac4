d17bc064ffe580e2022606ec3f20d341f295e7b3
Gerrit Grunwald <han.solo@mac.com>
2022-09-19 11:33:52 +0200

    cosmetics

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 echo "$(date +'%H:%M:%S.%3N') Call jar file"
 
 # Output epoch seconds before starting the application
-# java -XX:+UnlockDiagnosticVMOptions -Djdk.crac.trace-startup-time=true -XX:+CRTraceStartupTime -XX:CRaCCheckpointTo=/home/hansolo/crac-files -jar build/libs/crac4-17.0.0.jar
+# java -XX:+UnlockDiagnosticVMOptions -Djdk.crac.trace-startup-time=true -XX:+CRTraceStartupTime -XX:CRaCCheckpointTo=crac-files -jar build/libs/crac4-17.0.0.jar
 
 # Output compilation log that can be analyzed with JITWatch
-# java -XX:CRaCCheckpointTo=/home/hansolo/crac-files -XX:+UnlockDiagnosticVMOptions -XX:+LogCompilation -jar build/libs/crac4-17.0.0.jar
+# java -XX:CRaCCheckpointTo=crac-files -XX:+UnlockDiagnosticVMOptions -XX:+LogCompilation -jar build/libs/crac4-17.0.0.jar
 
 # Check whether crac-files folder is present not empty and if so, restore from checkpoint, otherwise start from jar
 if [ -d "crac-files" ]; then
